9b9602f6262def53e8c38666c7bcdae739fdf819
Adarsh K Sundaresan <adarshksundaresan395@gmail.com>
2025-10-27 08:59:37 +0530

    Refactor DFA logic to handle 'b' input and invalid cases

--- C/DFA/dfastart.c
+++ C/DFA/dfastart.c
@@ -1,56 +1,75 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    int state ='0';
-    char str[100];
-
-    printf("Enter the string need ");
-    scanf("%s",str);
-
-    for(int i=0;str[i]!='\0';i++)
-    {
-        switch(state)
-        {
-            case '0':
-                if(str[i] == 'a'){
-                    state = '1';
-                }
-                else{
-                    state = '3';
-                }
-                break;
-            case '1':
-                if(str[i] == 'a'){
-                    state = '2';
-                }
-                else{
-                    state = '3';
-                }
-                break;
-            case '2':
-                if(str[i] == 'a'){
-                    state = '2';
-                }
-                else {
-                    state = '2';
-                }
-                break;
-            case '3':
-                if(str[i] == 'a'){
-                    state = '3';
-                }
-                else {
-                    state = '3';
-                }
-                break;
-
-        }
-    }
-    if (state == '2')
-    {
-        printf("String is accepted\n");
-    }
-    else{
-        printf("String is not accepted\n");
-    }
+	int states = '0';
+	char str[100];
+	
+	printf("Enter the string:");
+	scanf("%s",str);
+	
+	for(int i=0;str[i]!='\0';i++)
+	{
+		switch(states)
+		{
+			case '0':
+				if(str[i]=='a'){
+				states = '1';
+				}
+				else if(str[i]=='b'){
+				states = '3';
+				}
+				else{
+				printf("Invalid\n");
+				exit(0);
+				}
+				break;
+			case '1':
+				if(str[i]=='a'){
+				states = '2';
+				}
+				else if(str[i]=='b')
+				{
+				states = '3';
+				}
+				else{
+				printf("Invalid\n");
+				exit(0);
+				}
+				break;
+			case '2':
+				if(str[i]=='a'){
+				states = '2';
+				}
+				else if(str[i]=='b'){
+				states = '2';
+				}
+				else{
+				printf("Invalid\n");
+				exit(0);
+				}
+				break;
+			case '3':
+                		if(str[i] == 'a'){
+                	    states = '3';
+               			}
+                		else if(str[i]=='b'){
+                    		states = '3';
+                		}
+                		else{
+				printf("Invalid\n");
+				exit(0);
+				}
+                		break;
+		}
+	}
+	
+	if(states =='2')
+	{
+		printf("Accepted\n");
+	}
+	else {
+		printf("Not accepted\n");
+	}
+	return 0;
 }
